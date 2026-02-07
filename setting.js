aac08b1a562a66cd24a5df207ca3c8d1eefb2a68
mowaiskhan147-afk <mowaiskhan147@gmail.com>
2026-02-07 18:57:58 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 M R _ O W A I S 𓆪',
+  author: '‎',
+  botName: "𓆩 M R _ O W A I S 𓆪",
+  botOwner: 'OWAIS', // Your name
+  ownerNumber: '923490770861', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/mowaiskhan147-afk/MR_OWAIS_bot",
+};
+
+module.exports = settings;
